64a711692d96fcee246212e2a4e1fc89ca12fbbf
Hotaru <biyuehuya@gmail.com>
2024-01-29 21:21:36 +0800

    feat: mcwss base on typescript

--- src/const.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const ALL_EVENTS: string[];
-export declare const PASS_EVENTS: string[];
-export declare const INVALID_EVENTS: string[];
-export declare const NOT_TESTED_EVENTS: string[];
